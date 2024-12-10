02f81e2181734fb0c4cd5047bc9dbbecf2cc4ee2
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-12-10 22:03:03 +0600

    Create json data for project card

---
+++ src/data/projects.js
@@ -0,0 +1,47 @@
+[
+    {
+        "id" : "1",
+        "tags": ["dom", "javascript"],
+        "image": "https://i.ibb.co.com/P59sdRF/quiz.png",
+        "title":"JavaScript Quiz Game",
+        "date": 'new Date()',
+        "stars": {
+          "href": "https://github.com/jharnakhatun2/quizeapp",
+          "totalStars": "65",
+        },
+        "description": "Quiz App is an interactive and dynamic web application that allows users to test their knowledge through a series of customizable quizzes.",
+        "tagsList": ["JS DOM", "JavaScript", "QuizApp", "CustomQuiz"],
+        "live_links": "https://quizeapp-six.vercel.app/",
+        "source_code" : "https://github.com/jharnakhatun2/quizeapp"
+      },
+    {
+        "id" : "2",
+        "tags": ["javascript","dom"],
+        "image": "https://i.ibb.co.com/tYDRd8T/form-Validation.png",
+        "title":"Dynamic Form Validation",
+        "date": 'new Date()',
+        "stars": {
+          "href": "https://github.com/jharnakhatun2/javascript-form-validation",
+          "totalStars": "65",
+        },
+        "description": "JavaScript Form Validation project implements real-time validation techniques to ensure that users input correct and complete information before submission.",
+        "tagsList": ["RealTimeValidation", "Form Validation", "JS DOM", "FormValidation"],
+        "live_links": "https://javascript-form-validation-pied.vercel.app/",
+        "source_code" : "https://github.com/jharnakhatun2/javascript-form-validation"
+      },
+    {
+        "id" : "3",
+        "tags": ["javascript","dom"],
+        "image": "https://i.ibb.co.com/r3XGkmd/framework-Manager.png",
+        "title":"Framework Manager",
+        "date": 'new Date()',
+        "stars": {
+          "href": "https://github.com/jharnakhatun2/framework-manager",
+          "totalStars": "65",
+        },
+        "description": "With a simple and intuitive interface, users can quickly add new frameworks or remove existing ones, ensuring their framework collection stays organized.",
+        "tagsList": ["Framework manager", "JavaScript", "JS DOM", "Web App"],
+        "live_links": "https://framework-manager.vercel.app/",
+        "source_code" : "https://github.com/jharnakhatun2/framework-manager"
+      },
+]
