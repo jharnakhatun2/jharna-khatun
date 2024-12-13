3ac6ba18c60779cb786030d870ee8e0f4f9ca489
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-12-13 20:43:09 +0600

    Modify blog page styles

---
+++ src/components/Blogs/Categories.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const categories = ['HTML', 'Tailwind', 'JavaScript', 'React.js', 'Next.js'];
+
+const Categories = () => {
+    return (
+        <div id="category-select" className="flex flex-wrap gap-3 py-2 text-sm">
+            {
+                categories.map(item => (
+                    <button
+                        value="html"
+                        className="sort-btn bg-zinc-500 py-1 px-2 rounded border border-zinc-500 hover:border-white hover:bg-opacity-0 text-white hover:text-primary transition-colors"
+                    >
+                        {item}
+                    </button>
+                ))
+            }
+
+        </div>
+    )
+};
+
+export default Categories;
