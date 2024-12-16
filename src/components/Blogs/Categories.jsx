71abd5e2ff4b72897aa42eb70e4fc74e6f7ad5f7
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-12-16 18:27:45 +0600

    update Blog categories map

--- src/components/Blogs/Categories.jsx
+++ src/components/Blogs/Categories.jsx
@@ -6,8 +6,8 @@ const Categories = () => {
     return (
         <div id="category-select" className="flex flex-wrap gap-3 py-2 text-sm">
             {
-                categories.map(item => (
-                    <button
+                categories.map((item, index) => (
+                    <button key={index}
                         value="html"
                         className="sort-btn bg-zinc-500 py-1 px-2 rounded border border-zinc-500 hover:border-white hover:bg-opacity-0 text-white hover:text-primary transition-colors"
                     >
